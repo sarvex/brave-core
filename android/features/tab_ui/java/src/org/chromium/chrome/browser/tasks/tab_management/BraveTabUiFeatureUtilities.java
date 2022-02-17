0fb835fa5975d0c1bda4a46c6686ac44bdcac9ae
Max <41635752+mkarolin@users.noreply.github.com>
2022-02-17 14:11:39 -0500

    Merge pull request #11829 from brave/cr99
    Upgrade from Chromium 98 to Chromium 99.

--- android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/BraveTabUiFeatureUtilities.java
+++ android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/BraveTabUiFeatureUtilities.java
@@ -5,43 +5,9 @@
 
 package org.chromium.chrome.browser.tasks.tab_management;
 
-import android.content.Context;
 
 import org.chromium.chrome.browser.flags.CachedFeatureFlags;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
-import org.chromium.chrome.browser.preferences.BravePreferenceKeys;
 import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 
 public class BraveTabUiFeatureUtilities {
-    /**
-     * @return Whether the Grid Tab Switcher UI is enabled and available for use.
-     */
-    public static boolean isGridTabSwitcherEnabled(Context context) {
-        if (!isTabGroupsAndroidEnabled(context)) {
-            return false;
-        }
-        return TabUiFeatureUtilities.isGridTabSwitcherEnabled(context);
-    }
-
-    /**
-     * @return Whether the tab group feature is enabled and available for use.
-     */
-    public static boolean isTabGroupsAndroidEnabled(Context context) {
-        // For backward compatibility we take value of Tab Grid feature if BRAVE_TAB_GROUPS_ENABLED
-        // setting hasn't been created. We don't want to rely on Tab Grid feature itself since it
-        // can be removed in the upstream going forward.
-        if (!SharedPreferencesManager.getInstance().contains(
-                    BravePreferenceKeys.BRAVE_TAB_GROUPS_ENABLED)) {
-            SharedPreferencesManager.getInstance().writeBoolean(
-                    BravePreferenceKeys.BRAVE_TAB_GROUPS_ENABLED,
-                    CachedFeatureFlags.isEnabled(ChromeFeatureList.TAB_GRID_LAYOUT_ANDROID));
-        }
-
-        if (!SharedPreferencesManager.getInstance().readBoolean(
-                    BravePreferenceKeys.BRAVE_TAB_GROUPS_ENABLED, true)) {
-            return false;
-        }
-
-        return TabUiFeatureUtilities.isTabGroupsAndroidEnabled(context);
-    }
 }
