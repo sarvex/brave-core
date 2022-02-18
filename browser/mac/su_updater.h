cf176e2239c21897337a638646280903a2252552
Michael Herrmann <michael@herrmann.io>
2022-02-18 16:23:23 +0100

    Fix lint errors

--- browser/mac/su_updater.h
+++ browser/mac/su_updater.h
@@ -1,9 +1,12 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_BROWSER_MAC_SU_UPDATER_H_
 #define BRAVE_BROWSER_MAC_SU_UPDATER_H_
+
+#if defined(__OBJC__)
 
 #import <Foundation/Foundation.h>
 
@@ -31,5 +34,7 @@
 @property BOOL automaticallyDownloadsUpdates;
 
 @end
+
+#endif  // __OBJC__
 
 #endif  // BRAVE_BROWSER_MAC_SU_UPDATER_H_
